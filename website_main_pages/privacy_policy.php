2604c8fd6136c860076c3b4babde69db307aaf84
liz603957 <32182168+liz603957@users.noreply.github.com>
2018-07-21 17:27:23 -0400

    Create privacy_policy.php

---
+++ website_main_pages/privacy_policy.php
@@ -0,0 +1,19 @@
+<?php
+ session_start();
+?>
+<html>
+<head>
+<title>Privacy Policy</title>
+</head>
+<body>
+<?php include '../template/template.php';?>
+<div class="container">
+ <div class="row">
+  <div>
+   <h3>Privacy Policy</h3>
+   <p>Page needs to be filled out</p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
