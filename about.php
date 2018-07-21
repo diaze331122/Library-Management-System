e71527d922c0836fc6808d1be4a49a4c8d61d1e9
liz603957 <32182168+liz603957@users.noreply.github.com>
2018-07-21 16:53:25 -0400

    Create about.php
    An about page that can be accessed through the footer of the website template.

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php
+ session_start();
+?>
+<html>
+<head>
+ <title>About Us</title>
+</head>
+<body>
+<?php include '../template/template.php';?>
+<div class="container">
+ <div class="row">
+  <div>
+   <h3>About Us!</h3>
+   <p>This website was created for the 60334 Final Project that implements a Library System.</p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
