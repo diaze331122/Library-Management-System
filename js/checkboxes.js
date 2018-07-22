18698a9aa776042fa9769437df8dc4af9225a8aa
liz603957 <32182168+liz603957@users.noreply.github.com>
2018-07-21 22:57:54 -0400

    Create checkboxes.js

---
+++ js/checkboxes.js
@@ -0,0 +1,18 @@
+var checkAll = document.getElementById("check_all_header");
+checkAll.addEventListener("change",check_all);
+
+function check_all(){
+ var checkboxes = document.getElementsByTagName("input");
+ var isChecked = false;
+ 
+ if (checkAll.checked){
+    isChecked = true;
+ }else{
+    isChecked = false;
+ }
+ for (var i=0;i<checkboxes.length;i++){
+   if (checkboxes[i].type == "checkbox"){
+      checkboxes[i].checked = isChecked;
+   }
+ }
+}
