fb0cf87c2ddaf69c5a9596e8d7fc74b2295d67a6
liz603957 <32182168+liz603957@users.noreply.github.com>
2018-08-13 04:57:51 -0400

    Add files via upload

---
+++ html/template/template.php
@@ -0,0 +1,22 @@
+<html>
+   <head>
+      <title>BookShare!</title>
+      <meta charset="utf-8"> 
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="../../js/JQuery/jquery-3.2.1.min.js"></script>
+      <script src="../../bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="../../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="../../css/template.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+   </head>
+   <body>
+   <?php include 'navigation_bar/index.php'; ?>
+   <footer>
+	<ul id="footer_links">
+	<li><a href="../pages/about.php">About Us </a></li>	
+	<li><a href="../pages/contact.php">Contact Us </a></li>			
+	<li><a href="../pages/privacy_policy.php">Privacy Policy </a></li>
+	</ul>			
+   </footer>
+   </body>
+</html>
